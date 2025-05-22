5a773446cf199483de0b5df256039143d20b7776
Zixi-Jiang <398940895@qq.com>
2025-05-22 15:14:40 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,31 +1,17 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
+import os
 import mysql.connector
 
-app = Flask(__name__)
-CORS(app)
-
 def get_patterns():
-    conn = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='Mysql:112358',  # 替换为你的MySQL密码
-        database='xilan_kapu'
-    )
+    db_config = {
+        'host': os.getenv('DATABASE_HOST', 'localhost'),
+        'user': os.getenv('DATABASE_USER', 'root'),
+        'password': os.getenv('DATABASE_PASSWORD', 'Mysql:112358'),
+        'database': os.getenv('DATABASE_NAME', 'xilan_kapu')
+    }
+    conn = mysql.connector.connect(**db_config)
     cursor = conn.cursor(dictionary=True)
     cursor.execute('SELECT name, type, features, symbolic_meaning, background_story FROM patterns')
     patterns = cursor.fetchall()
     cursor.close()
     conn.close()
     return patterns
-
-@app.route('/patterns', methods=['GET'])
-def patterns():
-    try:
-        patterns = get_patterns()
-        return jsonify(patterns)
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-if __name__ == '__main__':
-    app.run(debug=True)
